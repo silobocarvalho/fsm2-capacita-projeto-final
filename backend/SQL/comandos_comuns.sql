f127bdc6900dd9e669c5f91492cdf52bf6b19fb7
Sidartha <silobocarvalho@gmail.com>
2025-02-14 09:14:18 -0300

    add exemplos comandos sql

--- backend/SQL/comandos_comuns.sql
+++ backend/SQL/comandos_comuns.sql
@@ -21,3 +21,28 @@ INSERT INTO pokemons (nome, preco, tipo, raridade, url_img) VALUES
 ('Ekans', 39.00, 'Veneno', 'Semi-raro', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/23.png'),
 ('Charmander', 129.00, 'Fogo', 'Semi-raro', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png'),
 ('Mewtwo', 5000.00, 'Psiquico', 'Épico', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png')
+
+
+-- COMANDOS SQL EXEMPLOS
+SELECT * FROM pokemons WHERE preco > 399
+
+UPDATE pokemons SET preco=159 WHERE nome = 'Ekans'
+
+SELECT * FROM pokemons ORDER BY nome
+
+SELECT * FROM pokemons ORDER BY preco
+
+SELECT * FROM pokemons ORDER BY preco DESC
+
+SELECT * FROM pokemons LIMIT 3
+
+SELECT COUNT(*) AS numero_de_pokemons FROM pokemons
+
+SELECT MAX(preco) FROM pokemons
+
+SELECT MIN(preco) FROM pokemons
+
+SELECT AVG(preco) FROM pokemons
+
+-- Como resgatar o pokemon mais caro listando somente o Nome e o Preco dele.
+SELECT nome, preco FROM pokemons WHERE preco = (SELECT MAX(preco) FROM pokemons)
